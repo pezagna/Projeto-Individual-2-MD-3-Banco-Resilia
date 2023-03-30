0331fc5fb2b3b2378295138506dfe5d6d75314ac
Rob <123372510+pezagna@users.noreply.github.com>
2023-03-30 08:40:35 -0300

    Atualizando consultas.

--- Realizar  consultas no banco de dados/Realizar Consultas.sql
+++ Realizar  consultas no banco de dados/Realizar Consultas.sql
@@ -1,35 +1,36 @@
--- 1. Qual empresa utiliza o maior número de tecnologias na última pesquisa (2/2022)?	
-SELECT empresas.nome, COUNT(*) as num_tecnologias
-FROM relacao_empresas_tecnologias
-JOIN empresas ON empresas.id = relacao_empresas_tecnologias.empresa_id
+-- 1. Qual empresa utiliza o maior número de tecnologias na última pesquisa(2/2022)?
+	
+SELECT empresas.nome, COUNT(DISTINCT tecnologias.id) AS num_tecnologias
+FROM empresas
+JOIN relacao_empresas_tecnologias ON empresas.id = relacao_empresas_tecnologias.empresa_id
+JOIN tecnologias ON relacao_empresas_tecnologias.tecnologia_id = tecnologias.id
 JOIN relatorios ON relatorios.id = relacao_empresas_tecnologias.relatorio_id
-WHERE relatorios.data_compilacao = '2023-01-01' AND relacao_empresas_tecnologias.semestre = 2
+WHERE relatorios.data_compilacao = '2023-01-01' AND relatorios.data_compilacao <= '2023-07-30'
 GROUP BY empresas.id
 ORDER BY num_tecnologias DESC
 LIMIT 1;
-
--- 2. Qual empresa utilizava o menor número de tecnologias na pesquisaanterior (1/2022)?
+-- 2. Qual empresa utilizava o menor número de tecnologias na pesquisa anterior (1/2022)?
 SELECT empresas.nome, COUNT(*) as num_tecnologias
 FROM relacao_empresas_tecnologias
 JOIN empresas ON empresas.id = relacao_empresas_tecnologias.empresa_id
 JOIN relatorios ON relatorios.id = relacao_empresas_tecnologias.relatorio_id
-WHERE relatorios.data_compilacao = '2022-06-30' AND relacao_empresas_tecnologias.semestre = 1
+WHERE relatorios.data_compilacao >= '2022-07-01' AND relatorios.data_compilacao <= '2022-12-31'
 GROUP BY empresas.id
 ORDER BY num_tecnologias ASC
 LIMIT 1;
 
--- 3. Quantas empresas utilizam tecnologias da área de “Dados” atualmente? 
-SELECT COUNT(DISTINCT empresas.id) as num_empresas
+-- Quantas empresas utilizam tecnologias da área de “Dados” atualmente?
+SELECT COUNT(DISTINCT empresas.id) as numero_de_empresas
 FROM relacao_empresas_tecnologias
 JOIN empresas ON empresas.id = relacao_empresas_tecnologias.empresa_id
 JOIN tecnologias ON tecnologias.id = relacao_empresas_tecnologias.tecnologia_id
 JOIN relatorios ON relatorios.id = relacao_empresas_tecnologias.relatorio_id
-WHERE tecnologias.area = 'Ciência de Dados' AND relacao_empresas_tecnologias.semestre = 2 
+WHERE tecnologias.area = 'Ciência de Dados'
 
--- 4. Quantas empresas utilizam tecnologias que não são da área de “Dados” atualmente?
-SELECT COUNT(DISTINCT empresas.id) as num_empresas
+-- Quantas empresas utilizam tecnologias que não são da área de “Dados”atualmente?
+SELECT COUNT(DISTINCT empresas.id) as numero_de_empresas
 FROM relacao_empresas_tecnologias
 JOIN empresas ON empresas.id = relacao_empresas_tecnologias.empresa_id
 JOIN tecnologias ON tecnologias.id = relacao_empresas_tecnologias.tecnologia_id
 JOIN relatorios ON relatorios.id = relacao_empresas_tecnologias.relatorio_id
-WHERE tecnologias.area != 'Ciência de Dados' AND relacao_empresas_tecnologias.semestre = 2
+WHERE tecnologias.area != 'Ciência de Dados'
