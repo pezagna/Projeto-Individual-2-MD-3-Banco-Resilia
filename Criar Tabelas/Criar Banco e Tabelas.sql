710ccedfdb1312c97f94734d130585cc75b75e76
Rob <123372510+pezagna@users.noreply.github.com>
2023-03-30 21:06:05 -0300

    Atualizando comandos da criação do banco e tabelas

--- Criar Tabelas/Criar Banco e Tabelas.sql
+++ Criar Tabelas/Criar Banco e Tabelas.sql
@@ -1,19 +1,20 @@
--- Criar tabela de empresas parceiras
+-- Comando para criar a tabela de empresas parceiras
 CREATE TABLE empresas (
     id SERIAL PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    endereco VARCHAR(200),
-    telefone VARCHAR(20),
-    email VARCHAR(50)
+    Nome VARCHAR(100) NOT NULL,
+    Endereco VARCHAR(200),
+    Cnpj VARCHAR(14),
+	Telefone VARCHAR(20),
+    Email VARCHAR(50)
 );
 
--- Criar tabela de tecnologias
+-- Comando para criar a tabela de tecnologias
 CREATE TABLE tecnologias (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     area VARCHAR(50)
 );
--- Criar tabela de relacionamento entre Empresas e Tecnologias nos relatórios
+-- Comando para criar a tabela de relacionamento entre Empresas e Tecnologias que irão se relacionar com a tabela Relatórios
 CREATE TABLE relacao_empresas_tecnologias (
     id SERIAL PRIMARY KEY,
     empresa_id INTEGER NOT NULL,
@@ -23,8 +24,8 @@ CREATE TABLE relacao_empresas_tecnologias (
     FOREIGN KEY (tecnologia_id) REFERENCES tecnologias(id),
     FOREIGN KEY (relatorio_id) REFERENCES relatorios(id)
 );
--- Criar tabela de Relatórios
+-- Comando para criar a tabela de Relatórios
 CREATE TABLE relatorios (
     id SERIAL PRIMARY KEY,
-    data_compilacao DATE NOT NULL
+    data_do_relatorio DATE NOT NULL
 );
